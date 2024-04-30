6d058716f7a7bebbfaa17feac69a8de6457dcb45
unknown <shugavaneshwar13@gmail.com>
2024-04-30 12:07:50 +0530

    up

--- backend/server/apps/endpoints/serializer.py
+++ backend/server/apps/endpoints/serializer.py
@@ -6,5 +6,50 @@ from apps.endpoints.models import MLRequest
 
 class EndpointSerializer(serializers.ModelSerializer):
     class Meta:
-        model = ':) mudiyala naa thuga poran aporam pakalam ......'
+        model = Endpoint
+        read_only_fielsd = ("id","name","ower","created_at")
+        fields = read_only_fielsd
 
+class MLAlgorithmSerializer(serializers.ModelSerializer):
+    
+    current_status = serializers.SerializerMethodField(read_only=True)
+    
+    def get_current_status(self,mlalgorithm):
+        return(MLAlgorithmStatus.objects.filter(parent_mlalgorithm=mlalgorithm).latest('created_at').status)
+    
+    class Meta:
+        model= MLAlgorithm
+        read_only_fields=("id","name","description","code",
+                          "version","ower","cerated_at",
+                          "parent_endpoint","current_status")
+
+        fields= read_only_fields
+class MLAlgorithmStatusSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MLAlgorithmStatus
+        read_only_fields = ("id","active")
+        fields = ("id","active","status","created_by","created_at","parent_mlalgorithm")
+        
+class MLRequestSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MLRequest
+        read_only_fields = (
+            "id",
+            "input_data",
+            "full_response",
+            "response",
+            "created_at",
+            "parent_mlalgorithm"
+        )
+        fields = (
+            "id",
+            "input_data" ,
+            "full_response" , 
+            "response" , 
+            "feedback" ,
+            "created_at" ,
+            "parent_mlalgorithm" 
+        )
+     
+   
+        
